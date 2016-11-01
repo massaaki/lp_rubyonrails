3881ed56c9cd80fbc49266130878d8a52fe3fddc
Mauricio Massaaki Yochizumi <mauricio.massaaki@gmail.com>
2016-11-01 14:03:47 -0200

    Added Mailchimp Registration

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,22 +9,22 @@ class PagesController < ApplicationController
 		contact.email = params[:email].downcase
 		contact.type_user = "lead frio"	
 		if contact.save
-			# begin
-				# Gibbon::Request.api_key = ENV['API_MAILCHIMP']
-				# Gibbon::Request.timeout = 15
-				# gibbon = Gibbon::Request.new
+			begin
+				Gibbon::Request.api_key = ENV['API_MAILCHIMP']
+				Gibbon::Request.timeout = 15
+				gibbon = Gibbon::Request.new
 
 				#Make a member_id key as same algorith as mailchimp
-				# member_id = Digest::MD5.hexdigest(contact.email)
+				member_id = Digest::MD5.hexdigest(contact.email)
 
 				#Check if member exists update them
-				# if gibbon.lists('3e6b528408').members(member_id).retrieve 
-					# gibbon.lists("3e6b528408").members(member_id).update(body: {email_address: contact.email, status: "subscribed" })
-				# end
+				if gibbon.lists('3e6b528408').members(member_id).retrieve 
+					gibbon.lists("3e6b528408").members(member_id).update(body: {email_address: contact.email, status: "subscribed",interests: {"58431551ed": true} })
+				end
 
-			# rescue
-				# gibbon.lists("3e6b528408").members.create(body: {email_address: contact.email, status: "subscribed", interests: {"b1cf04f75a": true, "78a68d6bef": true} })
-			# end
+			rescue
+				gibbon.lists("3e6b528408").members.create(body: {email_address: contact.email, status: "subscribed", interests: {"b1cf04f75a": true, "58431551ed": true} })
+			end
 			# WelcomeLandingRegistration.delay.welcome(contact.email)
 			flash[:message] = 'Parabéns! Prepare-se para o dia 14/Nov, alguns dias antes enviaremos o seu acesso para o e-mail cadastrado'
 		else 
